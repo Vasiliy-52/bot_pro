e66676868d03fc488f8f888116d9738c9001aaa4
Vasiliy-52 <106384149+Vasiliy-52@users.noreply.github.com>
2022-07-19 14:07:44 +0300

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,11 @@
+import telebot
+
+TOKEN = "5479352740:AAE5DNBb6F8xHoPb02_kbwGMHjW30T9RL6I"
+bot = telebot.TeleBot(TOKEN)
+# @bot.message_handler(commands=['start','help']) - обрабатываем комманды
+#     def handle_start_help(message)
+
+
+@bot.message_handler(commands=['start','help'])
+def send_welcom(message):
+    bot.send_message(message.chat.id, f'Welcom, {message.chat.username}')
